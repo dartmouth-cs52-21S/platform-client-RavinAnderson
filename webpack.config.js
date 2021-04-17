6dc769bd4b9494e8fbc44bec0641171afbb4f58b
Ravin Anderson <ravin.j.anderson@gmail.com>
2021-04-16 22:02:09 -0400

    added better compression/optimization plugin

--- webpack.config.js
+++ webpack.config.js
@@ -16,24 +16,51 @@ module.exports = {
   devtool: 'source-map', // this enables debugging with source in chrome devtools
   module: {
     rules: [
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         useRelativePath: true,
+      //         name: '[name].[ext]',
+      //       },
+      //     },
+      //   ],
+      // },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     { loader: 'babel-loader' },
+      //   ],
+      // },
       {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              useRelativePath: true,
-              name: '[name].[ext]',
-            },
-          },
-        ],
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024,
+        },
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' },
-        ],
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024,
+          // Remove quotes around the encoded URL –
+          // they’re rarely useful
+          noquotes: true,
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre',
       },
       {
         test: /\.s?css/,
