4be0d2a68c04013e5c3ae6ef1507932af97c0132
Ravin Anderson <ravin.j.anderson@gmail.com>
2021-04-27 21:10:52 -0400

    starterpack update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // change require to es6 import style
 import $ from 'jquery';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './style.scss';
 
 let num = 0;
@@ -8,3 +9,11 @@ setInterval(() => {
   $('#main').html(`You've been on this page for ${num} seconds.`);
   num += 1;
 }, 1000);
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
